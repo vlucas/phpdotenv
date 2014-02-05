011ebb3c6778944540c1691f9ccfa71847197594
Stan Lemon <stosh1985@gmail.com>
2014-02-04 21:42:21 -0500

    Updates for PSR2

--- src/Dotenv.php
+++ src/Dotenv.php
@@ -11,28 +11,34 @@ class Dotenv
      */
     public static function load($path, $file = '.env')
     {
-        if(!is_string($file)) {
+        if (!is_string($file)) {
             $file = '.env';
         }
 
         $filePath = rtrim($path, '/') . '/' . $file;
-        if(!file_exists($filePath) || !is_file($filePath)) {
-            throw new \InvalidArgumentException("Dotenv: Environment file .env not found. Create file with your environment settings at " . $filePath);
+        if (!file_exists($filePath) || !is_file($filePath)) {
+            throw new \InvalidArgumentException(
+                sprintf(
+                    "Dotenv: Environment file .env not found. " .
+                    "Create file with your environment settings at %s",
+                    $filePath
+                )
+            );
         }
 
         // Read file into an array of lines with auto-detected line endings
         $autodetect = ini_get('auto_detect_line_endings');
-        ini_set( 'auto_detect_line_endings', '1' );
+        ini_set('auto_detect_line_endings', '1');
         $lines = file($filePath, FILE_SKIP_EMPTY_LINES);
-        ini_set( 'auto_detect_line_endings', $autodetect );
+        ini_set('auto_detect_line_endings', $autodetect);
 
-        foreach($lines as $line) {
+        foreach ($lines as $line) {
             // Only use non-empty lines that look like setters
-            if(strpos($line, '=') !== false) {
+            if (strpos($line, '=') !== false) {
                 // Strip quotes because putenv can't handle them. Also remove 'export' if present
                 $line = str_replace(array('export ', '\'', '"'), '', $line);
                 // Remove whitespaces around key & value
-                list( $key, $val ) = array_map( 'trim', explode('=', $line, 2) );
+                list($key, $val) = array_map('trim', explode('=', $line, 2));
 
                 putenv("$key=$val");
                 // Set PHP superglobals
@@ -52,18 +58,17 @@ class Dotenv
         $envs = (array) $env;
         $missingEnvs = array();
 
-        foreach($envs as $env) {
+        foreach ($envs as $env) {
             // Check $_SERVER in addition to ENV
-            if(!isset($_SERVER[$env]) || getenv($env) === false) {
+            if (!isset($_SERVER[$env]) || getenv($env) === false) {
                 $missingEnvs[] = $env;
             }
         }
 
-        if(!empty($missingEnvs)) {
+        if (!empty($missingEnvs)) {
             throw new \RuntimeException("Required ENV vars missing: '" . implode("', '", $missingEnvs) . "'");
         }
 
         return true;
     }
 }
-
