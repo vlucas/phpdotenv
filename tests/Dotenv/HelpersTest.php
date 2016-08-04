df434260e377b254b5e35cf95c2e84070a89a221
jordanbardsley7 <jordanbardsley7@yahoo.com>
2016-08-04 15:44:40 -0400

    already loaded dotenv

--- tests/Dotenv/HelpersTest.php
+++ tests/Dotenv/HelpersTest.php
@@ -3,26 +3,17 @@
 class HelpersTest extends PHPUnit_Framework_Testcase
 {
     /**
-     * Holds an environment.
-     *
-     * @var \Dotenv\Dotenv
-     */
-    private $env;
-
-    /**
      * Setup a new enviroment.
      *
      * @return void
      */
     public function setup()
     {
-        $this->env = create_env(__DIR__ . '/../fixtures/env');
+        create_env(__DIR__ . '/../fixtures/env');
     }
 
     public function testDotEnv()
     {
-        $this->env->load();
-
         $this->assertSame('bar', getenv('FOO'));
         $this->assertSame('baz', getenv('BAR'));
     }
