dcf435c15307f3c7750c20821b9a2b751bb29635
Andru Cherny <wir_wolf@bk.ru>
2017-07-17 17:06:50 +0300

    Create helpers.php

---
+++ src/helpers.php
@@ -0,0 +1,128 @@
+<?php
+
+if ( ! function_exists('env'))
+{
+    /**
+     * Gets the value of an environment variable. Supports boolean, empty and null.
+     *
+     * @param  string  $key
+     * @param  mixed   $default
+     * @return mixed
+     */
+    function env($key, $default = null)
+    {
+        $value = getenv($key);
+
+        if ($value === false) return value($default);
+
+        switch (strtolower($value))
+        {
+            case 'true':
+            case '(true)':
+                return true;
+
+            case 'false':
+            case '(false)':
+                return false;
+
+            case 'null':
+            case '(null)':
+                return null;
+
+            case 'empty':
+            case '(empty)':
+                return '';
+        }
+
+        if (str_starts_with($value, '"') && str_ends_with($value, '"'))
+        {
+            return substr($value, 1, -1);
+        }
+
+        return $value;
+    }
+}
+
+if ( ! function_exists('value'))
+{
+    /**
+     * Return the default value of the given value.
+     *
+     * @param  mixed  $value
+     * @return mixed
+     */
+    function value($value)
+    {
+        return $value instanceof Closure ? $value() : $value;
+    }
+}
+
+if ( ! function_exists('str_ends_with'))
+{
+    /**
+     * Determine if a given string ends with a given substring.
+     *
+     * @param  string  $haystack
+     * @param  string|array  $needles
+     * @return bool
+     */
+    function str_ends_with($haystack, $needles)
+    {
+        foreach ((array) $needles as $needle)
+        {
+            if ((string) $needle === substr($haystack, -strlen($needle))) return true;
+        }
+
+        return false;
+    }
+}
+
+if ( ! function_exists('str_starts_with'))
+{
+    /**
+     * Determine if a given string starts with a given substring.
+     *
+     * @param  string  $haystack
+     * @param  string|array  $needles
+     * @return bool
+     */
+    function str_starts_with($haystack, $needles)
+    {
+        foreach ((array) $needles as $needle)
+        {
+            if ($needle != '' && strpos($haystack, $needle) === 0) return true;
+        }
+
+        return false;
+    }
+}
+
+if ( ! function_exists('array_get'))
+{
+    /**
+     * Get an item from an array using "dot" notation.
+     *
+     * @param  array   $array
+     * @param  string  $key
+     * @param  mixed   $default
+     * @return mixed
+     */
+    function array_get($array, $key, $default = null)
+    {
+        if (is_null($key)) return $array;
+
+        if (isset($array[$key])) return $array[$key];
+
+        foreach (explode('.', $key) as $segment)
+        {
+            if ( ! is_array($array) || ! array_key_exists($segment, $array))
+            {
+                return value($default);
+            }
+
+            $array = $array[$segment];
+        }
+
+        return $array;
+    }
+}
