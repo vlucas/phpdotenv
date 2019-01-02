d6d94e9fc92a533cf06ff24fe5599aefba895fcf
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2019-01-02 20:46:57 +0000

    Reverted change to validator

--- src/Validator.php
+++ src/Validator.php
@@ -60,7 +60,7 @@ class Validator
     {
         return $this->assertCallback(
             function ($value) {
-                return strlen($value) > 0;
+                return strlen(trim($value)) > 0;
             },
             'is empty'
         );
