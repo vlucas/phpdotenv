0136b7a71f7496cc5402587b6fd4089f31a27c14
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2019-01-03 13:50:26 +0000

    Refactored parser and validate names (#311)

--- src/Parser.php
+++ src/Parser.php
@@ -34,6 +34,8 @@ class Parser
      *
      * @param string $line
      *
+     * @throws \Dotenv\Exception\InvalidFileException
+     *
      * @return array
      */
     private static function splitStringIntoParts($line)
@@ -45,6 +47,12 @@ class Parser
             list($name, $value) = array_map('trim', explode('=', $line, 2));
         }
 
+        if ($name === '') {
+            throw new InvalidFileException(
+                self::getErrorMessage('an unexpected equals', $line)
+            );
+        }
+
         return [$name, $value];
     }
 
@@ -53,15 +61,37 @@ class Parser
      *
      * @param string $name
      *
+     * @throws \Dotenv\Exception\InvalidFileException
+     *
      * @return string
      */
     private static function sanitiseName($name)
     {
-        return trim(str_replace(['export ', '\'', '"'], '', $name));
+        $name = trim(str_replace(['export ', '\'', '"'], '', $name));
+
+        if (!self::isValidName($name)) {
+            throw new InvalidFileException(
+                self::getErrorMessage('an invalid name', $name)
+            );
+        }
+
+        return $name;
     }
 
     /**
-     * Strips quotes from the environment variable value.
+     * Is the given variable name valid?
+     *
+     * @param string $name
+     *
+     * @return bool
+     */
+    private static function isValidName($name)
+    {
+        return preg_match('~\A[a-zA-Z0-9_.]+\z~', $name) === 1;
+    }
+
+    /**
+     * Strips quotes and comments from the environment variable value.
      *
      * @param string|null $value
      *
@@ -75,44 +105,75 @@ class Parser
             return $value;
         }
 
-        if (self::beginsWithAQuote($value)) { // value starts with a quote
-            $quote = $value[0];
-            $regexPattern = sprintf(
-                '/^
-                %1$s           # match a quote at the start of the value
-                (              # capturing sub-pattern used
-                 (?:           # we do not need to capture this
-                  [^%1$s\\\\]+ # any character other than a quote or backslash
-                  |\\\\\\\\    # or two backslashes together
-                  |\\\\%1$s    # or an escaped quote e.g \"
-                 )*            # as many characters that match the previous rules
-                )              # end of the capturing sub-pattern
-                %1$s           # and the closing quote
-                .*$            # and discard any string after the closing quote
-                /mx',
-                $quote
-            );
-            $value = preg_replace($regexPattern, '$1', $value);
-            $value = str_replace("\\$quote", $quote, $value);
-            $value = str_replace('\\\\', '\\', $value);
-        } else {
-            $parts = explode(' #', $value, 2);
-            $value = $parts[0];
+        if (self::beginsWithAQuote($value)) {
+            return self::processQuotedValue($value);
+        }
 
-            // Unquoted values cannot contain whitespace
-            if (preg_match('/\s+/', $value) > 0) {
-                // Check if value is a comment (usually triggered when empty value with comment)
-                if (preg_match('/^#/', $value) > 0) {
-                    $value = '';
-                } else {
-                    throw new InvalidFileException(
-                        'Dotenv values containing spaces must be surrounded by quotes.'
-                    );
-                }
+        // Strip comments from the left
+        $value = explode(' #', $value, 2)[0];
+
+        // Unquoted values cannot contain whitespace
+        if (preg_match('/\s+/', $value) > 0) {
+            // Check if value is a comment (usually triggered when empty value with comment)
+            if (preg_match('/^#/', $value) > 0) {
+                $value = '';
+            } else {
+                throw new InvalidFileException(
+                    self::getErrorMessage('an unexpected space', $value)
+                );
             }
         }
 
         return $value;
+    }
+
+    /**
+     * Strips quotes from the environment variable value.
+     *
+     * @param string $value
+     *
+     * @return string
+     */
+    private static function processQuotedValue($value)
+    {
+        $quote = $value[0];
+
+        $pattern = sprintf(
+            '/^
+            %1$s           # match a quote at the start of the value
+            (              # capturing sub-pattern used
+             (?:           # we do not need to capture this
+              [^%1$s\\\\]+ # any character other than a quote or backslash
+              |\\\\\\\\    # or two backslashes together
+              |\\\\%1$s    # or an escaped quote e.g \"
+             )*            # as many characters that match the previous rules
+            )              # end of the capturing sub-pattern
+            %1$s           # and the closing quote
+            .*$            # and discard any string after the closing quote
+            /mx',
+            $quote
+        );
+
+        $value = preg_replace($pattern, '$1', $value);
+
+        return str_replace('\\\\', '\\', str_replace("\\$quote", $quote, $value));
+    }
+
+    /**
+     * Generate a friendly error message.
+     *
+     * @param string $cause
+     * @param string $subject
+     *
+     * @return string
+     */
+    private static function getErrorMessage($cause, $subject)
+    {
+        return sprintf(
+            'Failed to parse dotenv file due to %s. Failed at [%s].',
+            $cause,
+            strtok($subject, "\n")
+        );
     }
 
     /**
