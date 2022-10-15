c349cad3c63eae4343ceaa0d404db173fc9867d1
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2022-10-15 23:28:12 +0100

    [4.3] Support PHP 8.2 (#542)

--- src/Regex/Regex.php
+++ src/Regex/Regex.php
@@ -88,9 +88,11 @@ class Regex
         $result = $operation($subject);
 
         if (($e = preg_last_error()) !== PREG_NO_ERROR) {
+            /** @var Result<V,string> */
             return Error::create(self::lookupError($e));
         }
 
+        /** @var Result<V,string> */
         return Success::create($result);
     }
 
