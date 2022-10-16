5561c43890a957c8e310de7859f186bef37fafbf
Graham Campbell <hello@gjcampbell.co.uk>
2022-10-16 01:47:20 +0100

    Fix issue where line ending in `="` is incorrectly marked as a multi-line start

--- src/Loader/Lines.php
+++ src/Loader/Lines.php
@@ -41,15 +41,17 @@ class Lines
      */
     private static function multilineProcess($multiline, $line, array $buffer)
     {
+        $startsOnCurrentLine = $multiline ? false : self::looksLikeMultilineStart($line);
+
         // check if $line can be multiline variable
-        if ($started = self::looksLikeMultilineStart($line)) {
+        if ($startsOnCurrentLine) {
             $multiline = true;
         }
 
         if ($multiline) {
             array_push($buffer, $line);
 
-            if (self::looksLikeMultilineStop($line, $started)) {
+            if (self::looksLikeMultilineStop($line, $startsOnCurrentLine)) {
                 $multiline = false;
                 $line = implode("\n", $buffer);
                 $buffer = [];
